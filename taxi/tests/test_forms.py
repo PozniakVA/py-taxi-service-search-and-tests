dcb30addbfdcf79f4d820fdc30291554c5c5919e
Valentin Pozniak <waliker448@gmail.com>
2024-06-10 22:51:27 +0300

    'Solution5'

--- taxi/tests/test_forms.py
+++ taxi/tests/test_forms.py
@@ -23,7 +23,10 @@ class FormsTests(TestCase):
         form = DriverLicenseUpdateForm(data={"license_number": license_number})
         self.assertTrue(form.is_valid())
         self.assertEqual(form.cleaned_data["license_number"], license_number)
-        self.assertEqual(len(form.cleaned_data["license_number"]), length_license_number)
+        self.assertEqual(
+            len(form.cleaned_data["license_number"]),
+            length_license_number
+        )
         self.assertTrue(
             form.cleaned_data["license_number"][:3].isalpha()
             and form.cleaned_data["license_number"][:3].isupper()
